7b56341976bc4f3bff29f3e1b87268d68bedb0ca
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-05-22 08:20:57 +0100

    added api info

--- src/main/java/com/project/blog/configuration/SpringFoxConfig.java
+++ src/main/java/com/project/blog/configuration/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.project.blog.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -15,6 +18,15 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return  new ApiInfoBuilder()
+                .title("Blog API")
+                .version("1.0")
+                .description("API for Blog Application")
+                .contact(new Contact("Odedoyin Akindele", "github.com/lapulgaatomica", "akindeleodedoyin@gmail.com"))
                 .build();
     }
 }
