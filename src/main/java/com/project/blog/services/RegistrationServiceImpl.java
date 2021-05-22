26d7e10d1ad25f26b21432e498c90123ba0c9328
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-05-22 10:22:31 +0100

    moved the registration implementation to RegistrationServiceImpl

--- src/main/java/com/project/blog/services/RegistrationServiceImpl.java
+++ src/main/java/com/project/blog/services/RegistrationServiceImpl.java
@@ -2,23 +2,34 @@ package com.project.blog.services;
 
 import com.project.blog.domain.BlogUser;
 import com.project.blog.dtos.RegisterRequest;
+import com.project.blog.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
 @Service
 public class RegistrationServiceImpl implements RegistrationService {
-
-    private final UserDetailsServiceImpl userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     @Override
     public String register(RegisterRequest registerRequest) {
         // Todo verify that email is valid
-        BlogUser user = userDetailsService.signUpUser(
-                new BlogUser(registerRequest.getUsername(),
-                        registerRequest.getEmail(),
-                        registerRequest.getPassword())
-        );
+
+        boolean usernameAlreadyExists = userRepository.findByUsername(registerRequest.getUsername()).isPresent();
+        boolean emailAlreadyExists = userRepository.findByEmail(registerRequest.getEmail()).isPresent();
+
+        if(usernameAlreadyExists || emailAlreadyExists){
+            throw new IllegalStateException("Username or Email is already taken");
+        }
+
+        BlogUser user = new BlogUser();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+
+        userRepository.save(user);
 
         return user.getEmail();
     }
--- src/main/java/com/project/blog/services/UserDetailsServiceImpl.java
+++ src/main/java/com/project/blog/services/UserDetailsServiceImpl.java
@@ -1,36 +1,20 @@
 package com.project.blog.services;
 
-import com.project.blog.domain.BlogUser;
 import com.project.blog.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Username " + username + " not found!"));
-    }
-
-
-    public BlogUser signUpUser(BlogUser user) {
-        boolean usernameAlreadyExists = userRepository.findByUsername(user.getUsername()).isPresent();
-        boolean emailAlreadyExists = userRepository.findByEmail(user.getEmail()).isPresent();
-
-        if(usernameAlreadyExists || emailAlreadyExists){
-            throw new IllegalStateException("Username or Email is already taken");
-        }
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-
-        return userRepository.save(user);
     }
 }
