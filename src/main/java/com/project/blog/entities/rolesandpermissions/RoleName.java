12dd3f0d4c276bf42a83b2f04517ea0ec7784d58
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-06-02 11:45:05 +0100

    added roles and permissions(granular)

--- src/main/java/com/project/blog/entities/rolesandpermissions/RoleName.java
+++ src/main/java/com/project/blog/entities/rolesandpermissions/RoleName.java
@@ -2,12 +2,28 @@ package com.project.blog.entities.rolesandpermissions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Getter
 public enum RoleName {
-    USER,
-    POST_MODERATOR,
-    COMMENT_MODERATOR,
-    SUPER_ADMIN;
+    USER(Set.of()),
+    POST_MODERATOR(Set.of(UserPermission.POST_WRITE)),
+    COMMENT_MODERATOR(Set.of(UserPermission.COMMENT_WRITE)),
+    SUPER_ADMIN(Set.of(UserPermission.POST_WRITE,
+            UserPermission.COMMENT_WRITE,
+            UserPermission.USER_WRITE));
+
+    private final Set<UserPermission> permissions;
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
 }
