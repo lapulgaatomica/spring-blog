5fbd86cc2916a672e8df3e22a9987573df16e402
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-06-13 17:38:22 +0100

    removed unwanted import  statement

--- src/main/java/com/project/blog/security/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/project/blog/security/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -16,7 +16,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
