c65e6aee4605d9b6c871dabd03e4ac358db655ab
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-06-21 16:58:11 +0100

    service tests

--- src/test/java/com/project/blog/services/CommentServiceTest.java
+++ src/test/java/com/project/blog/services/CommentServiceTest.java
@@ -1,96 +1,104 @@
-//package com.project.blog.services;
-//
-//import com.project.blog.entities.Comment;
-//import com.project.blog.entities.Post;
-//import com.project.blog.dtos.CommentRequest;
-//import com.project.blog.repositories.CommentRepository;
-//import com.project.blog.repositories.PostRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.ArgumentCaptor;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import java.time.LocalDateTime;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.verify;
-//
-//
-//@ExtendWith(MockitoExtension.class)
-//class CommentServiceTest {
-//    @Mock
-//    private CommentRepository commentRepository;
-//
-//    @Mock
-//    private PostRepository postRepository;
-//
-//    private CommentService commentService;
-//
-//    @BeforeEach
-//    void setUp(){
-//        commentService = new CommentServiceImpl(commentRepository, postRepository);
-//    }
-//
-//    @Test
-//    public void addNewComment(){
-//        // Given
-//        Post post = new Post(1L, "post title", "post", LocalDateTime.now(), null);
-//        CommentRequest commentDTO = new CommentRequest("content", 1L);
-//
-//        Comment comment = new Comment(1L, "content", LocalDateTime.now(), null, post);
-//        given(postRepository.findById(1L)).willReturn(Optional.of(post));
-//
-//        // When
-//        commentService.newComment(commentDTO);
-//
-//        // Then
-//        ArgumentCaptor<Comment> commentArgumentCaptor = ArgumentCaptor.forClass(Comment.class);
-//        verify(commentRepository).save(commentArgumentCaptor.capture());
-//        Comment capturedComment = commentArgumentCaptor.getValue();
-//        assertThat(capturedComment.getContent()).isEqualTo(commentDTO.getContent());
-//        assertThat(capturedComment.getPost().getId()).isEqualTo(commentDTO.getPostId());
-//    }
-//
-//    @Test
-//    public void canGetComment(){
-//        // Given
-//        Post post = new Post(1L, "post title", "post", LocalDateTime.now(), null);
-//        Comment comment = new Comment(1L, "content", LocalDateTime.now(), null, post);
-//        given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
-//
-//        // When
-//        commentService.getComment(1L);
-//
-//        // Then
-//        verify(commentRepository).findById(1L);
-//    }
-//
-//    @Test
-//    public void canUpdateComment(){
-//        // Given
-//        Post post = new Post(1L, "post title", "post", LocalDateTime.now(), null);
-//        CommentRequest commentDTO = new CommentRequest("content", 1L);
-//
-//        Comment comment = new Comment(1L, "content", LocalDateTime.now(), null, post);
-//        given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
-//
-//        // When
-//        commentService.updateComment(1L, commentDTO);
-//
-//        // Then
-//        assertThat(comment);
-//    }
-//
-//    @Test
-//    public void canDeleteComment(){
-//        // When
-//        commentService.deleteComment(1L);
-//
-//        // Then
-//        verify(commentRepository).deleteById(1L);
-//    }
-//}
+package com.project.blog.services;
+
+import com.project.blog.entities.BlogUser;
+import com.project.blog.entities.Comment;
+import com.project.blog.entities.Post;
+import com.project.blog.payloads.CommentRequest;
+import com.project.blog.repositories.BlogUserRepository;
+import com.project.blog.repositories.CommentRepository;
+import com.project.blog.repositories.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+
+@ExtendWith(MockitoExtension.class)
+class CommentServiceTest {
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Mock
+    private PostRepository postRepository;
+
+    @Mock
+    private BlogUserRepository userRepository;
+
+    private CommentService commentService;
+
+    @BeforeEach
+    void setUp(){
+        commentService = new CommentServiceImpl(commentRepository, postRepository, userRepository);
+    }
+
+    @Test
+    public void addNewComment(){
+        // Given
+        BlogUser user = new BlogUser();
+        Post post = new Post(1L, "post title", "post", LocalDateTime.now(), null, user);
+        CommentRequest commentRequest = new CommentRequest();
+
+        Comment comment = new Comment(1L, "content", post, user);
+        given(postRepository.findById(1L)).willReturn(Optional.of(post));
+
+        // When
+        commentService.newComment(1L, commentRequest);
+
+        // Then
+        ArgumentCaptor<Comment> commentArgumentCaptor = ArgumentCaptor.forClass(Comment.class);
+        verify(commentRepository).save(commentArgumentCaptor.capture());
+        Comment capturedComment = commentArgumentCaptor.getValue();
+        assertThat(capturedComment.getContent()).isEqualTo(commentRequest.getContent());
+//        assertThat(capturedComment.getPost().getId()).isEqualTo(commentRequest.getPostId());
+    }
+
+    @Test
+    public void canGetComment(){
+        // Given
+        BlogUser user = new BlogUser();
+        Post post = new Post(1L, "post title", "post", LocalDateTime.now(), null, user);
+        Comment comment = new Comment(1L, "content", post, user);
+        given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
+
+        // When
+        commentService.getComment(1L);
+
+        // Then
+        verify(commentRepository).findById(1L);
+    }
+
+    @Test
+    public void canUpdateComment(){
+        // Given
+        BlogUser user = new BlogUser();
+        Post post = new Post(1L, "post title", "post", LocalDateTime.now(), null, user);
+        CommentRequest commentRequest = new CommentRequest();
+
+        Comment comment = new Comment(1L, "content", post, user);
+        given(commentRepository.findById(1L)).willReturn(Optional.of(comment));
+
+        // When
+        commentService.updateComment(1L, commentRequest);
+
+        // Then
+        assertThat(comment);
+    }
+
+    @Test
+    public void canDeleteComment(){
+        // When
+        commentService.deleteComment(1L);
+
+        // Then
+        verify(commentRepository).deleteById(1L);
+    }
+}
--- src/test/java/com/project/blog/services/PostServiceTest.java
+++ src/test/java/com/project/blog/services/PostServiceTest.java
@@ -1,98 +1,104 @@
-//package com.project.blog.services;
-//
-//import com.project.blog.entities.Post;
-//import com.project.blog.dtos.PostRequest;
-//import com.project.blog.repositories.CommentRepository;
-//import com.project.blog.repositories.PostRepository;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.verify;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class PostServiceTest {
-//
-//    @Mock
-//    private PostRepository postRepository;
-//
-//    @Mock
-//    private CommentRepository commentRepository;
-//    private PostService postService;
-//
-//    @BeforeEach
-//    void setUp() {
-//        postService = new PostServiceImpl(postRepository, commentRepository);
-//    }
-//
-//    @Test
-//    public void getBlogPosts(){
-//        // When
-//        postService.getBlogPosts();
-//
-//        // Then
-//        verify(postRepository).findAll();
-//    }
-//
-//    @Test
-//    public void getBlogPostWithComment(){
-//        long id =  1;
-//        PostRequest postDTO = new PostRequest("title", "blog post");
-//        Post post = new Post(null, postDTO.getTitle(), postDTO.getContent(), LocalDateTime.now(), null);
-//        // Given
-//        given(postRepository.findById(id)).willReturn(Optional.of(post));
-//        given(commentRepository.findByPostId(id)).willReturn(List.of());
-//
-//        // When
-//        postService.getBlogPostWithComment(1L);
-//
-//        // Then
-//        verify(postRepository).findById(1L);
-//    }
-//
-//    @Test
-//    public void newBlogPost(){
-//        // Given
-//        PostRequest postDTO = new PostRequest("title","blog post");
-//        Post post = new Post(null, postDTO.getTitle(), postDTO.getContent(), LocalDateTime.now(), null);
-//
-//        // When
-//        postService.newBlogPost(postDTO);
-//
-//        // Then
-//        verify(postRepository).save(post);
-//    }
-//
-//    @Test
-//    public void updateBlogPost(){
-//        long id =  1;
-//        PostRequest postDTO = new PostRequest("title", "blog post");
-//        Post post = new Post(null, postDTO.getTitle(), postDTO.getContent(), LocalDateTime.now(), null);
-//        // Given
-//        given(postRepository.findById(id)).willReturn(Optional.of(post));
-//
-//        // When
-//        postService.updateBlogPost(id, postDTO);
-//
-//        //
-//        assertThat(post);
-//    }
-//
-//    @Test
-//    public void deleteBlogPost(){
-//        // When
-//        postService.deleteBlogPost(1L);
-//
-//        // Then
-//        verify(postRepository).deleteById(1L);
-//    }
-//}
+package com.project.blog.services;
+
+import com.project.blog.entities.BlogUser;
+import com.project.blog.entities.Post;
+import com.project.blog.payloads.PostRequest;
+import com.project.blog.repositories.BlogUserRepository;
+import com.project.blog.repositories.CommentRepository;
+import com.project.blog.repositories.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private BlogUserRepository userRepository;
+    private PostService postService;
+
+    @BeforeEach
+    void setUp() {
+        postService = new PostServiceImpl(postRepository, commentRepository, userRepository);
+    }
+
+    @Test
+    public void getBlogPosts(){
+        // When
+        postService.getBlogPosts();
+
+        // Then
+        verify(postRepository).findAll();
+    }
+
+    @Test
+    public void newBlogPost(){
+        // Given
+        PostRequest postRequest = new PostRequest("title","blog post");
+        BlogUser user = new BlogUser();
+        Post post = new Post(null, postRequest.getTitle(), postRequest.getContent(), LocalDateTime.now(), null, user);
+
+        // When
+        postService.newBlogPost(postRequest);
+
+        // Then
+        verify(postRepository).save(post);
+    }
+
+    @Test
+    public void getBlogPostWithComment(){
+        long id =  1;
+        PostRequest postDTO = new PostRequest("title", "blog post");
+        BlogUser user = new BlogUser();
+        Post post = new Post(null, postDTO.getTitle(), postDTO.getContent(), LocalDateTime.now(), null, user);
+        // Given
+        given(postRepository.findById(id)).willReturn(Optional.of(post));
+        given(commentRepository.findByPostId(id)).willReturn(List.of());
+
+        // When
+        postService.getBlogPostWithComment(1L);
+
+        // Then
+        verify(postRepository).findById(1L);
+    }
+
+    @Test
+    public void updateBlogPost(){
+        long id =  1;
+        PostRequest postDTO = new PostRequest("title", "blog post");
+        BlogUser user = new BlogUser();
+        Post post = new Post(null, postDTO.getTitle(), postDTO.getContent(), LocalDateTime.now(), null, user);
+        // Given
+        given(postRepository.findById(id)).willReturn(Optional.of(post));
+
+        // When
+        postService.updateBlogPost(id, postDTO);
+
+        //
+        assertThat(post);
+    }
+
+    @Test
+    public void deleteBlogPost(){
+        // When
+        postService.deleteBlogPost(1L);
+
+        // Then
+        verify(postRepository).deleteById(1L);
+    }
+}
