55f37af79ed44691aaf498b53a2b8d868f6a0651
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-07-23 22:05:39 +0100

    added getRoles, changeRoles and changePassword tests for userService

--- src/test/java/com/project/blog/services/UserServiceTest.java
+++ src/test/java/com/project/blog/services/UserServiceTest.java
@@ -3,6 +3,8 @@ package com.project.blog.services;
 import com.project.blog.entities.BlogUser;
 import com.project.blog.entities.Role;
 import com.project.blog.entities.enums.RoleName;
+import com.project.blog.payloads.ChangeRoleRequest;
+import com.project.blog.payloads.PasswordChangeRequest;
 import com.project.blog.payloads.RegistrationRequest;
 import com.project.blog.repositories.BlogUserRepository;
 import com.project.blog.repositories.PasswordResetTokenRepository;
@@ -19,9 +21,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Optional;
 
 import static com.project.blog.entities.enums.RoleName.SUPER_ADMIN;
+import static com.project.blog.entities.enums.RoleName.USER;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -40,6 +44,8 @@ class UserServiceTest {
 
     @Captor
     private ArgumentCaptor<BlogUser> userArgumentCaptor;
+
+
 
     private UserService userService;
 
@@ -76,14 +82,48 @@ class UserServiceTest {
 
     @Test
     void getRoles() {
+        // When
+        userService.getRoles();
+
+        // Then
+        verify(roleRepository).findAll();
     }
 
     @Test
     void changeRole() {
+        // Given
+        Role role = new Role(SUPER_ADMIN);
+        BlogUser user = new BlogUser("username", "email@email.com", "password");
+        user.setRole(new Role(USER));
+        ChangeRoleRequest request = new ChangeRoleRequest();
+        request.setRole(SUPER_ADMIN);
+        given(blogUserRepository.findByUsername(user.getUsername())).willReturn(Optional.of(user));
+        given(roleRepository.findByName(request.getRole())).willReturn(Optional.of(role));
+
+        // When
+        userService.changeRole(user.getUsername(), request);
+
+        // Then
+        then(blogUserRepository).should().save(userArgumentCaptor.capture());
+        BlogUser userArgumentCaptorValue = userArgumentCaptor.getValue();
+        assertThat(userArgumentCaptorValue).usingRecursiveComparison().isEqualTo(user);
     }
 
     @Test
     void changePassword() {
+        // Given
+        BlogUser user = new BlogUser("username", "email@email.com", "password");
+        PasswordChangeRequest request = new PasswordChangeRequest("old", "new", "new");
+        given(blogUserRepository.findById(1L)).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(request.getOldPassword(), user.getPassword())).willReturn(true);
+
+        // When
+        userService.changePassword(1L, request, user.getUsername());
+
+        // Then
+        then(blogUserRepository).should().save(userArgumentCaptor.capture());
+        BlogUser userArgumentCaptorValue = userArgumentCaptor.getValue();
+        assertThat(userArgumentCaptorValue).usingRecursiveComparison().isEqualTo(user);
     }
 
     @Test
